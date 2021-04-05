3ffe22cb06d74e9fedb384557831948feb4bfc1b
Daniel Jung Dong <2926076+danieljungdong@users.noreply.github.com>
2021-04-04 17:32:33 -0700

    initial setup

---
+++ webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/client/index.js',
+    target: 'node',
+    output: {
+        // path: path.join(__dirname, 'dist'),
+        // filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    mode: 'development',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+            , {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+}
